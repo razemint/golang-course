0c6d7a9421aa39202be4b235698a585e56a7290a
Christian Martínez <JChristianMartinezG@Gmail.com>
2023-02-02 03:56:13 -0600

    Create time.go

---
+++ time.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	time.Sleep(time.Second * 2)
+	fmt.Println("Hello, World!")
+	fmt.Println("The time is", time.Now())
+}
+
+// End of snippet
